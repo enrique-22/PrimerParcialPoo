a820ab95c696902ae1b76c63c92af4cc9dd84f58
Rafael <Rafael@DESKTOP-HGMJVH3>
2018-05-03 22:45:12 -0600

    se agrego la lista para la clase listaReserva

--- src/parte/de/carlos/ListaReserva.java
+++ src/parte/de/carlos/ListaReserva.java
@@ -6,7 +6,6 @@
 package parte.de.carlos;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -16,19 +15,43 @@ import java.util.Scanner;
 
 public class ListaReserva {
     public ArrayList<Reserva> reservas;
-    public ArrayList<Cliente> clientes;
-    public int ver_por_semana;
-    public int cancelar;
-    public int modificar;
+    public ListaClientes clientes;
+    //public int ver_por_semana;
+    //public int cancelar;
+    //public int modificar;
     
     
-    public ListaReserva(){}
+    public ListaReserva(){
+    reservas= new ArrayList<>();
+    }
     
      public void nuevaReserva(){
         Reserva reserva=new Reserva();
-        nuevoCliente();
+        Scanner leer= new Scanner(System.in);
+         System.out.println("ingrese nobre del cliente:");
+        if (buscarCliente(leer.nextLine())){
+            Cliente nuevo= new Cliente();
+           clientes.add(nuevo);
+           reserva.cliente=nuevo;
+            System.out.println("desea agregar un paquete s/n");
+            String opcion=leer.next();
+            if (opcion=="n"){
+           reserva.pack=agregarPack;
+                   }
+            reserva.Total=reserva.habitacion.precio + reserva.pack.precio; 
+        }else System.out.println("el cliente ya tiene una reservacion");
+        
+        
         
     }
+     public boolean buscarCliente(String cliente){
+         for (Reserva e : reservas){
+             if (e.cliente.nombre==cliente){
+                 return false;
+             }
+         }
+         return true;
+     }
 
     public ListaReserva(ArrayList<Reserva> reservas) {
         this.reservas = reservas;
@@ -36,6 +59,19 @@ public class ListaReserva {
     public void nuevoCliente(){
         Scanner leer=new Scanner(System.in);
         
+    }
+    public Pack agregarPack(ListaPack lista){
+        System.out.println("selleccione su paquete a agregar");
+        lista.mostrar();
+        Utils leer = new Utils();
+        int i=leer.pedirEntero();
+        int flag=1;
+        for (Pack e : lista){
+            if (i==flag){
+                return e;
+            }
+        }
+        //return NULL;
     }
 }
 
